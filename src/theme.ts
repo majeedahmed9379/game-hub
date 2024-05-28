3225badcdd9b10de12d18d6ebf82f665d4c7feb7
Abdul Majeed <majeedahmed9379@gmail.com>
2024-05-28 13:16:12 +0500

    Built color mode switch and added transition

--- src/theme.ts
+++ src/theme.ts
@@ -2,8 +2,19 @@ import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
+  disableTransitionOnChange: false,
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  styles: {
+    global: {
+      body: {
+        transitionProperty: "all",
+        transitionDuration: "slow",
+      },
+    },
+  },
+});
 
 export default theme;
